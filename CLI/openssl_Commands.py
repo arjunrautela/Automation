08271bdd0e72cc4f9ac2843be047a3a0259f32c2
Arjun Singh Rautela <arjunrautela@gmail.com>
2018-08-30 15:49:21 +0530

    working example

--- CLI/openssl_Commands.py
+++ CLI/openssl_Commands.py
@@ -11,7 +11,7 @@ try:
     print(cli_handle)
     cli_handle.expect_data('OpenSSL>')
     cli_handle.send_data('version') #Cheking version of openssl
-    cli_handle.expect_data('OpenSSL 1.1.0g  2 Nov 2018')
+    cli_handle.expect_data('OpenSSL 1.1.0g  2 Nov 2017')
     # Generate a random hex string of length 16
     cli_handle.send_data('rand -hex 16')
 except CliException as e:
